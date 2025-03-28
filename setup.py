052379baf13da39cc33270ff89be0d9194a24550
naaa <na0908nna@gmail.com>
2025-03-28 15:56:52 +0300

    see all commit
    and shadcn

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NileCare AI ChatBot',
+    version='0.0.1',
+    author='NileCare-Anwar',
+    author_email='alanwarmam@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
