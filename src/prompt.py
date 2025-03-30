67f45c4044a1b618321b56443d3b448dd6192c49
naaa <na0908nna@gmail.com>
2025-03-30 04:06:56 +0300

    initializing OpenAI model - commit

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt=(
+    "You are an assistant question-answer task."
+    "use the following pieces of retrieved context to answer the question at the end."
+    "the question. if you dont know the answer , say that you"
+    "don't know.use three sentences maximum and keep the  "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
